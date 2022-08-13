8f455b7364aa649558206972aae60a3f6751dbc2
rugved-i <rugved.inamdar@amla.io>
2022-08-13 10:24:05 +0530

    Reverse Triangle

--- src/TrianglePrint.java
+++ src/TrianglePrint.java
@@ -3,13 +3,13 @@ import java.util.Arrays;
 public class TrianglePrint {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-//            if (i % 2 != 0) {
-                for (int j = 0; j < i; j++) {
-                    System.out.print("*");
-                }
-                System.out.println("");
-//            }
+        for (int i = 10; i >= 1; i--) {
+            if (i % 2 != 0) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+            }
         }
     }
 
